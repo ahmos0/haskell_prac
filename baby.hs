fa5094f302ac0e0f2bfe7e7945387cc78e2f1d00
ahmos <kamaboko_0228@icloud.com>
2022-12-29 14:47:19 +0900

    二章が終わった

--- baby.hs
+++ baby.hs
@@ -1,10 +1,31 @@
-doubleMe  x = x + x
+doubleMe x = x + x
+
 doubleUs x y = doubleMe x + doubleMe y
-doubleSmallNumber x = if x > 100
-                        then x
-                        else x * 2
-doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
-conanO'Brien = "It's a-me, Conan O'Brien!" --　関数を大文字で始められない
+
+doubleSmallNumber x =
+  if x > 100
+    then x
+    else x * 2
+
+doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
+
+conanO'Brien = "It's a-me, Conan O'Brien!" -- 　関数を大文字で始められない
+
 boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x] -- odd関数は奇数が与えられたらTrue
-length' xs = sum[1| _ <- xs]
-removeNonUppercase st = [c | c <- st, c `elem` ['A'.. 'Z']]
+
+length' xs = sum [1 | _ <- xs]
+
+removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase st = [c | c <- st, c `elem` ['A' .. 'Z']]
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+factorial :: Integer -> Integer
+factorial n = product [1 .. n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
